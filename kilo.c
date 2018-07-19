7ea239cf02fcebe5363fcbe964f8cf566c98f382
m3m0n <m3m0n@xenity.net>
2018-07-19 17:07:22 -0400

    Completed Step 7&8: Disable canonical mode, this way characters are passed to program as they are entered instead upon a new line marker. Second, enable displaying entered characters by printing them on screen as they are received by program

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,7 +15,7 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,7 +24,10 @@ int main (void) {
     enableRawMode();
     
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q') {
+        if (iscntrl(c)) printf("%d\n", c);
+        else printf("%d ('%c')\n", c, c);
+    }
 
     return 0;
 }
