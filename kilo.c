fda733cd25bfd7df03da12ae59ba50a7220bb347
m3m0n <m3m0n@xenity.net>
2018-07-19 16:46:20 -0400

    Complete step 5, Turn off ECHO flag to terminal. Starting to get from canonical mode to raw mode

--- kilo.c
+++ kilo.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
 
+void enableRawMode() { 
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main (void) {
+    enableRawMode();
+    
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q');
 
