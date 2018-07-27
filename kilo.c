92cf11e772dc681984eef0971ad240df3d758d95
m3m0n <m3m0n@xenity.net>
2018-07-27 15:36:11 -0400

    Step 90: Fix the way PgUp and PgDown work. Move cursor to top or bottom of screen before scrolling a full page

--- kilo.c
+++ kilo.c
@@ -1,7 +1,7 @@
 /***
-		 * kilo.c
- 	* 20/07/18
- 		* Shaun Memon
+ * kilo.c
+ * 20/07/18
+ * Shaun Memon
  * https://viewsourcecode.org/snaptoken/kilo/
  ***/
 
@@ -470,6 +470,13 @@ void editorProcessKeypress() {
         case PAGE_UP:
         case PAGE_DOWN:
             {
+                if (c==PAGE_UP)
+                    E.cy = E.rowoff;
+                else if (c==PAGE_DOWN) {
+                    E.cy = E.rowoff + E.screenrows -1;
+                    if (E.cy > E.numrows) E.cy = E.numrows;
+                }
+
                 int times = E.screenrows;
                 while (times--)
                     editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
